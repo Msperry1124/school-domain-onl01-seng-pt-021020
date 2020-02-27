9f64819768d2028edc9ac87b9864ef40da34dfef
Melissa Sperry <sperry.mel@gmail.com>
2020-02-27 03:35:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,14 +3,23 @@ class School
   def initialize(name)
     @name = name
     @roster = {}
-  end
+  end 
   def add_student(student, level)
     roster[level] ||= []
     roster[level] << student
-  end
+  end 
   def grade(level)
-    roster.detect do |x, y|
+    roster.detect do |x, y| 
       if x == level
-        return y
-      end
+        return y 
+      end 
     end 
+  end
+def sort 
+  nu_hash = {}
+  roster.each do |x, y| 
+    nu_hash[x] = y.sort 
+  end 
+  nu_hash
+end 
+end 
