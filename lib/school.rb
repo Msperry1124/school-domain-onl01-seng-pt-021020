c2d5b0cc767ba4030e38f6019f1ca03fa14745f3
Melissa Sperry <sperry.mel@gmail.com>
2020-02-27 03:35:24 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,16 @@
 class School
-  attr_accessor :name,
-  :roster
+  attr_accessor :name, :roster
   def initialize(name)
     @name = name
     @roster = {}
-  end
-
-  
+  end 
+  def add_student(student, level)
+    roster[level] ||= []
+    roster[level] << student
+  end 
+  def grade(level)
+    roster.detect do |x, y| 
+      if x == level
+        return y 
+      end 
+    end 
